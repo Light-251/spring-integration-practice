0c7851853e48172f133e73316b029dc64cc371cf
Silvio Le Pera <s.lepera@software-inside.it>
2024-08-14 20:21:54 +0200

    Resolved merged conflicts

--- src/main/java/it/silvio/spring_integration_practice/config/IntegrationConfig.java
+++ src/main/java/it/silvio/spring_integration_practice/config/IntegrationConfig.java
@@ -7,14 +7,36 @@ import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
+/**
+ * Configuration class for Spring Integration.
+ *
+ * This class defines the configuration for the Spring Integration framework.
+ */
 @Configuration
 public class IntegrationConfig {
 
+    /**
+     * Creates a direct message channel.
+     *
+     * This channel is used to send and receive messages within the application.
+     *
+     * @return a direct message channel
+     */
     @Bean
     public MessageChannel messageChannel() {
         return new DirectChannel();
     }
 
+    /**
+     * Creates a message handler for processing orders.
+     *
+     * This handler is triggered when a message is sent to the
+     * "discountedOrderChannel".
+     * It prints a message to the console indicating that an order is being
+     * processed.
+     *
+     * @return a message handler for processing orders
+     */
     @Bean
     @ServiceActivator(inputChannel = "discountedOrderChannel")
     public MessageHandler orderHandler() {
